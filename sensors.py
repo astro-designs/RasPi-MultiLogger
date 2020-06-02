f9cf93d826685a881dc7dcabb9d2fd1af33f1990
Mark Cantrill <mark@astro-designs.com>
2020-06-02 17:51:31 +0100

    Updating example sensors.py Monitors 13 sensors every 30 seconds and relays data to Domoticz host

--- sensors.py
+++ sensors.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 # Sensor configuration...
-SensorName = ["Kitchen Temperature", "Ping 192.168.1.254", "Ping 192.168.1.252", "Ping 192.168.1.253", "Ping 192.168.1.250", "192.168.1.179", "CPU Throttle", "CPU Throttle %", "192.168.1.174"]
-SensorType = ['T1w','Ping','Ping','Ping','Ping', 'Ping','Throttle_Status','Throttle_Level','Ping']
+# Note - Each array below must be equal in length to len(SensorName)
+
+SensorName = ["Kitchen Temperature", "CPU Throttle", "CPU Throttle %", "Ping 192.168.1.249", "Ping 192.168.1.250", "Ping 192.168.1.251", "Ping 192.168.1.252", "Ping 192.168.1.253", "Ping 192.168.1.254", "Ping 192.168.1.124", "Ping 192.168.1.138", "Ping 192.168.1.58", "Ping 192.168.1.185"]
+SensorType = ['T1w','Throttle_Status','Throttle_Level','Ping','Ping','Ping','Ping', 'Ping','Ping','Ping','Ping','Ping','Ping']
 # 'Throttle_Status' = CPU Throttle status (Current throttle status)
 # 'Throttle_Level' = CPU Throttle level (% time throttled)
 # 'CPU_Temp' = CPU temperature
@@ -12,16 +14,16 @@ SensorType = ['T1w','Ping','Ping','Ping','Ping', 'Ping','Throttle_Status','Throt
 # 'Ping' = network ping success (%)
 # 'LDRPin' = Analogue Light Dependant Resistor
 #SensorLoc = ['28-01191b9257fd','x','x','x']
-SensorLoc = ['10-000801503137','192.168.1.254','192.168.1.252','192.168.1.253','192.168.1.250','192.168.1.179','x','x','192.168.1.174']
-HighWarning = [125,125,125,125,125,125,125,125,125]
-HighReset = [0,0,0,0,0,0,0,0,0]
-LowWarning = [-25,-25,-25,-25,-25,-25,-25,-25,-25]
-LowReset = [0,0,0,0,0,0,0,0,0]
-DomoticzIDX = ['8','37','38','39','41','42','x','40','43','x'] # Use 'x' to disable logging to Domoticz for each sensor
+SensorLoc = ['10-000801503137','x','x','192.168.1.249','192.168.1.250','192.168.1.251','192.168.1.252','192.168.1.253','192.168.1.254','192.168.1.124','192.168.1.138','192.168.1.58','192.168.1.185']
+HighWarning = [125,125,125,125,125,125,125,125,125,125,125,125,125]
+HighReset = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+LowWarning = [-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25]
+LowReset = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+DomoticzIDX = ['8','40','43','48','41','49','38','39','37','44','45','46','47'] # Use 'x' to disable logging to Domoticz for each sensor
 
 ActiveSensors = len(SensorName)
 
-MeasurementInterval = 5
+MeasurementInterval = 30
 
 # Identify which sensor (if any) should be displayed to the local display
 DisplaySensor1 = -1 # Set to -1 to disable display
