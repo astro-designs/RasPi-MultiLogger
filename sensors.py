adaaed6728ee2f68621accfbcb2e81ac30ea57e5
Mark Cantrill <mark@astro-designs.com>
2023-12-26 14:31:36 +0000

    Adding calibration constants

--- sensors.py
+++ sensors.py
@@ -3,8 +3,8 @@
 # Sensor configuration...
 # Note - Each array below must be equal in length to len(SensorName)
 
-SensorName = ["Kitchen Temperature", "CPU Throttle", "CPU Throttle %", "Ping 192.168.1.249", "Ping 192.168.1.250", "Ping 192.168.1.251", "Ping 192.168.1.252", "Ping 192.168.1.253", "Ping 192.168.1.254", "Ping 192.168.1.124", "Ping 192.168.1.138", "Ping 192.168.1.58", "Ping 192.168.1.185"]
-SensorType = ['T1w','Throttle_Status','Throttle_Level','Ping','Ping','Ping','Ping', 'Ping','Ping','Ping','Ping','Ping','Ping']
+SensorName = ["Outside Temperature"]
+SensorType = ['T1w']
 # 'Throttle_Status' = CPU Throttle status (Current throttle status)
 # 'Throttle_Level' = CPU Throttle level (% time throttled)
 # 'CPU_Temp' = CPU temperature
@@ -17,13 +17,23 @@ SensorType = ['T1w','Throttle_Status','Throttle_Level','Ping','Ping','Ping','Pin
 # 'Electric_kW' = Electricity usage meter (Watts now)
 # 'SolarPV_Whrs_gen_today' = Solar PV generation meter (daily)
 # 'SolarPV_W' = Solar PV generation meter (Watts now)
-#SensorLoc = ['28-01191b9257fd','x','x','x']
-SensorLoc = ['10-000801503137','x','x','192.168.1.249','192.168.1.250','192.168.1.251','192.168.1.252','192.168.1.253','192.168.1.254','192.168.1.124','192.168.1.138','192.168.1.58','192.168.1.185']
-HighWarning = [125,125,125,125,125,125,125,125,125,125,125,125,125]
-HighReset = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-LowWarning = [-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25]
-LowReset = [0,0,0,0,0,0,0,0,0,0,0,0,0]
-DomoticzIDX = ['8','40','43','48','41','49','38','39','37','44','45','46','47'] # Use 'x' to disable logging to Domoticz for each sensor
+
+SensorLoc = ['28-01191b9257fd']
+
+# Sensor output calculation (gain & offset)
+# Output = Ax^2 + Bx + C
+Sensor_A = [0.0]
+Sensor_B = [1.0]
+Sensor_C = [0.0]
+
+# Sensor Error & Warning thresholds
+HighWarning = [125]
+HighReset = [0]
+LowWarning = [-25]
+LowReset = [0]
+
+# Domoticz config
+DomoticzIDX = ['63'] # Use 'x' to disable logging to Domoticz for each sensor
 
 # Other options
 
