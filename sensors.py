1551cc10e3f8a9bee3821f1861174e5d9c78033d
Mark Cantrill <mark@astro-designs.com>
2024-07-13 07:45:31 +0100

    Updating default / initial sensor configuration / options

--- sensors.py
+++ sensors.py
@@ -3,11 +3,21 @@
 # Sensor configuration...
 # Note - Each array below must be equal in length to len(SensorName)
 
+ModuleName = 'MultiLogger'
+ModuleLoc = 'MultiLogger'
 SensorName = ["Outside Temperature"]
 SensorType = ['T1w']
+SensorUnits = ['°C']
+# 'T1w' = One-Wire Temperature Sensor 
+# 'PIR' = Passive infra-red sensor
+# 'DHTxx_T' = DHT22 or DHT11 Temperature
+# 'DHTxx_H' = DHT22 or DHT11 Humidity
+# 'Vi' = Simple voltage measurement using internal ADC 
+
 # 'Throttle_Status' = CPU Throttle status (Current throttle status)
 # 'Throttle_Level' = CPU Throttle level (% time throttled)
 # 'CPU_Temp' = CPU temperature
+# 'Int_Temp' = local / internal temperature
 # 'T1w' = One-Wire Temperature Sensor 
 # 'LM75' = LM75 I2C Temperature Sensor
 # 'TPin' = Analogue Thermistor
@@ -27,12 +37,14 @@ Sensor_B = [1.0]
 Sensor_C = [0.0]
 
 # Sensor Error & Warning thresholds
-HighWarning = [125]
-HighReset = [0]
-LowWarning = [-25]
-LowReset = [0]
+# Set Warning and Reset thresholds to 0 to disable
+HighWarning = [0, 0, 0]
+HighReset   = [0, 0, 0]
+LowWarning  = [0, 0, 0]
+LowReset    = [0, 0, 0]
 
 # Domoticz config
+Domoticz_En = True
 DomoticzIDX = ['63'] # Use 'x' to disable logging to Domoticz for each sensor
 
 # Other options
@@ -41,8 +53,15 @@ DomoticzIDX = ['63'] # Use 'x' to disable logging to Domoticz for each sensor
 ActiveSensors = len(SensorName)
 
 # Measurement interval in seconds
-MeasurementInterval = 30
+MeasurementInterval = 60
 
-# Identify which sensor (if any) should be displayed to the local display
+# Log interval in seconds
+LogInterval = 30
+
+# Number of Readings to capture
+# Set to 0 to run continuously
+NumReadings = 100
+
+# Identify which sensor (if any) should be displayed to the local display, if present
 DisplaySensor1 = -1 # Set to -1 to disable display
 
